4e0a6c1cc186d72d245dc436adc72ab3c98024f8
vivian.yu <vivianttjojo@gmail.com>
2015-02-03 12:24:28 +0800

    runnable

--- src/com/thread/Actor.java
+++ src/com/thread/Actor.java
@@ -35,6 +35,40 @@ public class Actor extends Thread {
 		Thread actor = new Actor();
 		actor.setName("vivian.yu");
 		actor.start();
+
+		Thread actress = new Thread(new Actress(), "Efren.yang");
+		actress.start();
+	}
+
+}
+
+class Actress implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println("Name is : " + Thread.currentThread().getName() + " begin");
+		int count = 0;
+		boolean keepRun = true;
+
+		while (keepRun) {
+			System.out.println(Thread.currentThread().getName() + " is NO : " + count++);
+
+			if (count == 100)
+				keepRun = false;
+
+			if (count % 10 == 0) {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+		}
+
+		System.out.println("Name is : " + Thread.currentThread().getName() + " is over");
 	}
 
 }
